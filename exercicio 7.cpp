9bff1a4d1be2298c9b804e9bb374d4fb83d0c174
Lucas Melo <166817886+LCODE0@users.noreply.github.com>
2024-05-29 19:07:49 -0300

    Add files via upload

---
+++ exercicio 7.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+   vector<int> num(10);
+  int maior;
+  int menor;
+  
+
+  for(int i = 0; i < 10; i++){
+    cout << "digite o numero dos 10 vetores: ";
+    cin >> num[i];
+  }
+  
+  maior = num[0];
+  menor = num[0];
+
+  for(int i = 0; i < 10; i++){
+    if(num[i] > maior) {
+      maior = num[i];
+    } 
+
+    if(num[i] < menor) {
+      menor = num[i];
+    }
+  }
+
+  for(int i = 0; i < 10; i++){
+    cout << "A posicao " << i << " do vetor e: " << num[i] << endl;
+  }
+
+  cout << "O maior numero do vetor e: " << maior << endl;
+  cout << "O menor numero do vetor e: " << menor << endl;
+ 
+  
+}
